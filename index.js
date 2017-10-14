1fb723f6034df6e7677ffcd7fe9e76826ca7b89d
Dinesh Dabhi <dinesh.dabhi@rapidops.com>
2017-10-14 06:41:20 +0530

    Changes for adding the paging support.

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ let sqlite3 = require('sqlite3').verbose(),
 	response = new Response(),
 	bodyParser = require('body-parser');
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(bodyParser.json());
 app.use(response.handler());
 require('./models/users')(app,db);
--- models/users.js
+++ models/users.js
@@ -1,16 +1,16 @@
 module.exports = function (app, db) {
 	//insert data in users table
 	db.serialize(()=>{
-		db.run("CREATE TABLE users (id NUMBER,firstName TEXT,lastName TEXT,userName TEXT,email TEXT,age NUMBER)");
+		db.run("CREATE TABLE users (id NUMBER,firstName TEXT,lastName TEXT,userName TEXT,email TEXT,age TEXT)");
 		var stmt = db.prepare("INSERT INTO users VALUES (?,?,?,?,?,?)");
-		for (var i = 1; i <= 50; i++) {
+		for (var i = 1; i <= 5000; i++) {
 			let text = 'abcdefghijklmnopqrstuvwxyz',
 				textLength = text.length,
 				firstNameRandom = text.charAt(Math.floor(Math.random() * textLength)),
 				lastNameRandom = text.charAt(Math.floor(Math.random() * textLength)),
 				userNameRandom = text.charAt(Math.floor(Math.random() * textLength)),
 				emailRandom = text.charAt(Math.floor(Math.random() * textLength)),
-				ageRandom =  Math.floor(Math.random() * 70);
+				ageRandom =  Math.floor(Math.random() * 70).toString();
 			stmt.run(i,`First Name ${firstNameRandom}`,`Last Name ${lastNameRandom}`,`User Name ${userNameRandom}`,`email-${emailRandom}-@gmail.com`,ageRandom);
 		}
 		stmt.finalize();
@@ -20,7 +20,7 @@ module.exports = function (app, db) {
 	app.get('/api/users', (req, res) => {
 		let users = [];
 			sortParams = {sortBy:req.query.sortBy,sortOrder:req.query.sortOrder || 'asc'};
-			sortQuery = sortParams.sortBy?`order by ${sortParams.sortBy} ${sortParams.sortOrder}`:'order by id desc',
+			sortQuery = sortParams.sortBy?`order by ${sortParams.sortBy} ${sortParams.sortOrder}`:'order by id asc',
 			whereQuery = '',
 			page = req.query.page || 1,  //page number
 			rows = req.query.rows || 10, //rows per page
@@ -39,7 +39,7 @@ module.exports = function (app, db) {
 			whereQueryArray.push(`email like "%${req.query.email}%"`)
 		}
 		if(req.query.age){
-			whereQueryArray.push(`age=${req.query.age}`)
+			whereQueryArray.push(`age like "%${req.query.age}%"`)
 		}
 		if(whereQueryArray.length){
 			whereQuery = ' where '+whereQueryArray.join(' and ');
@@ -47,8 +47,9 @@ module.exports = function (app, db) {
 		if(page>1){
 			offset = rows*(page-1)
 		}
-		query = `SELECT * FROM users ${whereQuery} ${sortQuery}`/* limit ${rows} offset ${offset}`*/;
-		db.get(`select count(*) from users`,function(err,count){
+		query = `SELECT * FROM users ${whereQuery} ${sortQuery} limit ${rows} offset ${offset}`;
+		let countQuery = `select count(*) from users ${whereQuery}`;
+		db.get(countQuery,function(err,count){
 			if(err){
 				res.sendError(err);
 			} else {
@@ -102,7 +103,7 @@ module.exports = function (app, db) {
 	app.put('/api/users/:id', (req, res) => {
 		let userId = req.params.id;
 		let userData = req.body;
-		db.run(`update users set firstName="${userData.firstName}",lastName="${userData.lastName}",userName="${userData.userName}",email="${userData.email}",age=${userData.age} where id=${userId}`,
+		db.run(`update users set firstName="${userData.firstName}",lastName="${userData.lastName}",userName="${userData.userName}",email="${userData.email}",age="${userData.age}" where id=${userId}`,
 			(err,response) => {
 				if(err){
 					res.sendError(err);
